4c281574362e007adc0b871d347c72404404ef6a
Jorge Collazo <jcollazo151@west-mec.org>
2020-03-13 15:26:08 -0700

    Enter key doesn't work

--- case2/mt_calc.js
+++ case2/mt_calc.js
@@ -36,7 +36,7 @@
 window.onload = init();
 //sets up event handlers
 function init(){
-   var calcButtons = document.getElementsByClassName("calcButtons");
+   var calcButtons = document.getElementsByClassName("calcButton");
    for(var i = 0; i < calcButtons.length; i++){
       //runs the function buttonClick on click of the calcButtons
       calcButtons[i].addEventListener("click", buttonClick);
@@ -46,17 +46,17 @@ function init(){
 
 function buttonClick(e){
    var calcValue = document.getElementById("calcWindow").value;
-   var calcDecimal = document.getElementsByTagName("decimals").value;
+   var calcDecimal = document.getElementById("decimals").value;
    var buttonValue = e.target.value;
    switch(buttonValue){
       case "del":
          calcValue = "";
          break
       case "bksp":
-         calcValue += eraseChar(calcValue);
+         calcValue = eraseChar(calcValue);
          break
       case "enter":
-         calcValue += " = " + evalEq(calcValue, calcDecimal) + "\n";
+         calcValue += "=" + evalEq(calcValue, calcDecimal) + "\n";
          break
       case "prev":
          calcValue += lastEq(calcValue);
@@ -65,26 +65,27 @@ function buttonClick(e){
          calcValue += buttonValue;
          break
    }
-   calcValue = document.getElementById("calcWindow").value;
+   document.getElementById("calcWindow").value = calcValue;
+
    document.getElementById("calcWindow").focus();
 }
 
 function calcKeys(e){
    var calcValue = document.getElementById("calcWindow").value;
-   var calcDecimal = document.getElementsByTagName("decimals").value;
+   var calcDecimal = document.getElementById("decimals").value;
    switch(e.keys){
       case "Delete":
          calcValue = "";
          break;
       case "Enter":
-         calcValue += " = " + evalEq(calcValue, calcDecimal);
+         calcValue += "=" + evalEq(calcValue, calcDecimal);
          break;
       case "ArrowUp":
          calcValue += lastEq(calcWindow);
          e.preventDefault();
          break;
    }
-   calcWindow = 
+   document.getElementById("calcWindow").value = calcValue;
 }
 
 /* ===================================================================== */
